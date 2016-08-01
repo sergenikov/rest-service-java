6ea76ab8fd4bc639e245b479e4984d6416e4f01b
Sergey Skovorodnikov <sg.skov@gmail.com>
2016-08-01 11:34:32 -0700

    Added post method and test creating doctor in the db.

--- src/main/java/com/sergey/restclinic/resources/DoctorResource.java
+++ src/main/java/com/sergey/restclinic/resources/DoctorResource.java
@@ -3,31 +3,38 @@ package com.sergey.restclinic.resources;
 import com.mongodb.BasicDBObject;
 import com.mongodb.Block;
 import com.mongodb.DBCursor;
+import com.mongodb.MongoException;
+import com.mongodb.MongoWriteConcernException;
+import com.mongodb.MongoWriteException;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.sergey.restclinic.database.DatabaseConnection;
 import com.sergey.restclinic.models.Doctor;
 import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
 @Path("/doctors")
 public class DoctorResource {
-    
+        
     @GET
-    @Path("getdoctors")
+    @Path("getall")
     @Produces(MediaType.APPLICATION_XML)
     public List<Doctor> getDoctors() {
         
         DatabaseConnection db = DatabaseConnection.getInstance();
         
-        final List<Doctor> doctors = new ArrayList<Doctor>();
+        final List<Doctor> doctors = new ArrayList<>();
         
         FindIterable<Document> iterable = db.mongodb.getCollection("Doctor").find();
         iterable.forEach(new Block<Document>() {
@@ -43,11 +50,11 @@ public class DoctorResource {
     }
     
     @GET
-    @Path("getdoctorbyname")
+    @Path("getbyname")
     @Produces(MediaType.APPLICATION_XML)
     public List<Doctor> getDoctorByName2(@QueryParam("name") String name) {
         
-        final List<Doctor> doctors = new ArrayList<Doctor>();
+        final List<Doctor> doctors = new ArrayList<>();
         
         DatabaseConnection db = DatabaseConnection.getInstance();
         MongoCollection<Document> docCollection = db.mongodb.getCollection("Doctor");
@@ -69,6 +76,39 @@ public class DoctorResource {
         return doctors;
     }
     
+    /**
+     * Adding duplicates is allowed.
+     * @param name name of the doctor to be added
+     * @return Response with http codes and info message
+     */
+    @POST
+    @Path("add")
+    public Response addDoctor(String name) {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        MongoCollection<Document> docCollection = db.mongodb.getCollection("Doctor");
+        
+        // TODO when done validate fields/params received
+        
+        System.out.println("DoctorResource.addDoctor() adding doctor " + name);
+        Document newDoc = new Document();
+        newDoc.append("name", name);
+        try {
+            docCollection.insertOne(newDoc);
+        } catch (MongoWriteConcernException e) {
+            Response.status(500)
+                    .entity("Adding doctor failed; name=" + name + "; error " + e)
+                    .build();
+            System.err.println(e);
+        } catch (MongoException e) {
+            Response.status(500)
+                    .entity("Adding doctor failed; name=" + name + "; error " + e)
+                    .build();
+            System.err.println(e);
+        }
+        
+        return Response.status(200).entity("Success. Added doctor " + name).build();
+    }
+
     // A test call to see if api is working
     @GET
     @Path("getphil")
